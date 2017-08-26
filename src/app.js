355d46c026a2999a1e0a0e91368d7ea07007459f
Michael Chen <maochen201411@gmail.com>
2017-08-26 19:11:51 +0800

    add splash screen

--- src/app.js
+++ src/app.js
@@ -5,8 +5,17 @@ import {
   Text,
   View
 } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 
 export default class mml extends Component {
+
+  componentDidMount() {
+    	// do stuff while splash screen is shown
+        // After having done stuff (such as async tasks) hide the splash screen
+        SplashScreen.hide();
+    }
+
+    
   render() {
     return (
       <View style={styles.container}>
