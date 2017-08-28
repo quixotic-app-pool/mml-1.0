75e332f241a8b6db9e9e7e48aef877bad0d2a237
Michael Chen <maochen201411@gmail.com>
2017-08-28 15:48:08 +0800

    add discovery

---
+++ src/discoverySection/discoveryItem.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+} from 'react-native';
+
+export default class DiscoveryItem extends Component {
+    render(){
+
+      const { data } = this.props;
+      return(
+        <View>
+          <View>
+            <Image source={require('../dummyData/img/user.png')} />
+          </View>
+          <View>
+            <Text>{ data.user.name }</Text>
+            <Text>{ data.textBody }</Text>
+            <Image source={require('../dummyData/img/timg.png')} />
+          
+          </View>
+        </View>
+      )
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+
+  searchContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  }
+
+})
--- src/discoverySection/discoveryPage.js
+++ src/discoverySection/discoveryPage.js
@@ -4,13 +4,65 @@ import {
     Text,
     StyleSheet,
 } from 'react-native';
+import  DiscoveryItem from './discoveryItem';
+import PTRView from 'react-native-pull-to-refresh';
+
 
 export default class DiscoveryScreen extends Component {
+
+  _refresh() {
+   return new Promise((resolve) => {
+     setTimeout(()=>{resolve()}, 1000)
+   });
+ }
+
+
     render(){
+      const data = [
+        {
+          _id: Math.round(Math.random() * 1000000),
+          textBody: '今天的天气真明媚！',
+          imageBody: '../dummyData/img/timg.png',
+          createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+          user: {
+            _id: 1,
+            name: '王学姣',
+            avatar: '../dummyData/img/user.png',}
+        },
+        {
+          _id: Math.round(Math.random() * 1000000),
+          textBody: 'This is a wonderful day!',
+          imageBody: '../dummyData/img/timg.png',
+          createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+          user: {
+            _id: 2,
+            name: '王学姣',
+            avatar: '../dummyData/img/user.png',}
+        },
+        {
+          _id: Math.round(Math.random() * 1000000),
+          textBody: 'nothing but boring feeling~ aha',
+          imageBody: '../dummyData/img/timg.png',
+          createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+          user: {
+            _id: 3,
+            name: '王学姣',
+            avatar: '../dummyData/img/user.png',}
+        }]
+
       return(
-        <View>
-          <Text>discovery</Text>
-        </View>
+         <PTRView onRefresh={this._refresh} >
+            <View>
+            {data.map((item, i) => {
+                      return (
+                         <DiscoveryItem
+                           data = { item }
+                         />
+                      )
+                    })
+           }
+            </View>
+          </PTRView>
       )
     }
 }
